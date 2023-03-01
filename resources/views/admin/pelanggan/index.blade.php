e5f20e1fd1f57759273dd62402421bae4b373e6a
Muhammad Jilan Satria <jilanstria@gmail.comgit config --globar user.name jilanOmit --global to set the identity only it this repositorygit config --global user.email jilanstria@gmail.com>
2023-03-01 15:10:40 +0700

    form pelanggan

--- resources/views/admin/pelanggan/index.blade.php
+++ resources/views/admin/pelanggan/index.blade.php
@@ -1,57 +1,181 @@
 @extends('layouts.main')
 @section('konten')
-<div class="content-body">
-    <!-- row -->
-    <div class="container-fluid">
-        <div class="row page-titles">
-            <ol class="breadcrumb">
-                <li class="breadcrumb-item active"><a href="javascript:void(0)">Pelanggan</a></li>
-                <li class="breadcrumb-item"><a href="javascript:void(0)">List</a></li>
-            </ol>
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-header">
-                        <h4 class="card-title">Data Pelanggan</h4>
-                        <a href="" class="btn btn-rounded btn-primary"><i class="fas fa-plus"></i> Tambah</a>
-                    </div>
-                    <div class="card-body">
-                        <div class="table-responsive">
-                            <table id="example3" class="display" style="min-width: 845px">
-                                <thead>
-                                    <tr>
-                                        <th>No</th>
-                                        <th>Nama</th>
-                                        <th>Alamat</th>
-                                        <th>Jenis Kelamin</th>
-                                        <th>Telephone</th>
-                                        <th>Aksi</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1</td>
-                                        <td>asd</td>
-                                        <td>asd</td>
-                                        <td>asd</td>
-                                        <td>asd</td>
-                                        <td>
-                                            <div class="d-flex">
-                                                <a href="#" class="btn btn-primary shadow btn-xs sharp me-1"><i
-                                                        class="fas fa-pencil-alt"></i></a>
-                                                <a href="#" class="btn btn-danger shadow btn-xs sharp"><i
-                                                        class="fa fa-trash"></i></a>
-                                            </div>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
+    <div class="content-body">
+        <!-- row -->
+        <div class="container-fluid">
+            <div class="row page-titles">
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item active"><a href="javascript:void(0)">Pelanggan</a></li>
+                    <li class="breadcrumb-item"><a href="javascript:void(0)">List</a></li>
+                </ol>
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-header">
+                            <h4 class="card-title">Data Pelanggan</h4>
+                            <a onClick="add()" href="javascript:void(0)" class="btn btn-rounded btn-primary"><i class="fas fa-plus"></i> Tambah</a>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table id="example3" class="display" style="min-width: 845px">
+                                    <thead>
+                                        <tr>
+                                            <th>No</th>
+                                            <th>Nama</th>
+                                            <th>Alamat</th>
+                                            <th>Jenis Kelamin</th>
+                                            <th>Telephone</th>
+                                            <th>Aksi</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>asd</td>
+                                            <td>asd</td>
+                                            <td>asd</td>
+                                            <td>asd</td>
+                                            <td>
+                                                <div class="d-flex">
+                                                    <a href="#" class="btn btn-primary shadow btn-xs sharp me-1"><i
+                                                            class="fas fa-pencil-alt"></i></a>
+                                                    <a href="#" class="btn btn-danger shadow btn-xs sharp"><i
+                                                            class="fa fa-trash"></i></a>
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
+        {{-- modal create --}}
+        <div class="modal fade" id="modal-create" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+            aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Tambah Outlet</h1>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <div class="card-body">
+                            <div class="basic-form">
+                                <form action="javascript:void(0)" id="PelangganForm" name="PelangganForm">
+                                    <div class="row">
+                                        <div class="mb-3 col-md-12">
+                                            <label class="form-label">Nama Outlet</label>
+                                            <input type="text" name="nama" id="inputNama" class="form-control"
+                                                placeholder="Nama Outlet">
+                                        </div>
+                                        <div class="mb-3 col-md-12">
+                                            <label class="form-label">Alamat</label>
+                                            <input type="text" name="alamat" id="inputAlamat" class="form-control"
+                                                placeholder="Alamat">
+                                        </div>
+                                        <div class="mb-3 col-md-12">
+                                            <label class="form-label">No Telephone</label>
+                                            <input type="text" name="tlp" id="inputTlp" class="form-control"
+                                                placeholder="No Telephone">
+                                        </div>
+                                        <div class="mb-3 col-md-12">
+                                            <label class="form-label">Jenis Kelamin</label>
+                                            <input type="text" name="tlp" id="inputTlp" class="form-control"
+                                                placeholder="Jenis Kelamin">
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                            id="btnCreateClose">Batal</button>
+                        <button type="button" class="btn btn-primary" id="btnCreateSimpan">Simpan</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        {{-- end modal create --}}
+
     </div>
-</div>
 @endsection
+
+@push('ajax_crud')
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+
+    <script type="text/javascript">
+        function add() {
+            $('#PelangganForm').trigger("reset");
+            $('#CompanyModal').html("Add Company");
+            $('#modal-create').modal('show');
+            $('#id').val('');
+        }
+
+        function editFunc(id) {
+            $.ajax({
+                type: "POST",
+                url: "{{ url('edit-company') }}",
+                data: {
+                    id: id
+                },
+                dataType: 'json',
+                success: function(res) {
+                    $('#CompanyModal').html("Edit Company");
+                    $('#modal-create').modal('show');
+                    $('#id').val(res.id);
+                    $('#name').val(res.name);
+                    $('#address').val(res.address);
+                    $('#email').val(res.email);
+                }
+            });
+        }
+
+        function deleteFunc(id) {
+            if (confirm("Delete Record?") == true) {
+                var id = id;
+                // ajax
+                $.ajax({
+                    type: "POST",
+                    url: "{{ url('delete-company') }}",
+                    data: {
+                        id: id
+                    },
+                    dataType: 'json',
+                    success: function(res) {
+                        var oTable = $('#ajax-crud-datatable').dataTable();
+                        oTable.fnDraw(false);
+                    }
+                });
+            }
+        }
+        $('#PelangganForm').submit(function(e) {
+            e.preventDefault();
+            var formData = new FormData(this);
+            $.ajax({
+                type: 'POST',
+                url: "{{ url('store-company') }}",
+                data: formData,
+                cache: false,
+                contentType: false,
+                processData: false,
+                success: (data) => {
+                    $("#modal-create").modal('hide');
+                    var oTable = $('#ajax-crud-datatable').dataTable();
+                    oTable.fnDraw(false);
+                    $("#btn-save").html('Submit');
+                    $("#btn-save").attr("disabled", false);
+                },
+                error: function(data) {
+                    console.log(data);
+                }
+            });
+        });
+    </script>
+@endpush
